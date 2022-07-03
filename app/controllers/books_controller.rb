20dd472fe2504efafa19e02d6564d120d71a46db
EC2 Default User <ec2-user@ip-172-31-19-174.ap-northeast-1.compute.internal>
2022-07-03 12:09:37 +0000

    [add]3-12 投稿機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,30 @@
 class BooksController < ApplicationController
   def index
     @book = Book.new
+    @books = Book.all
+    
+  end
+
+  def create
+    # 1.2データを受け取り、インスタンス変数に保存
+    book = Book.new(book_params)
+    # 3.データベースを保存するためのsaveメソッド
+    book.save
+    # 4。index画面へリダイレクト
+    redirect_to book_path(book.id)
   end
 
   def show
+    
   end
 
   def edit
   end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
