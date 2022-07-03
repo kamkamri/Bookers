be909e4f4821af18192c6ef38667ce6058e45d14
EC2 Default User <ec2-user@ip-172-31-19-174.ap-northeast-1.compute.internal>
2022-07-03 22:42:34 +0000

    [add]3-12 createメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,9 +9,12 @@ class BooksController < ApplicationController
     # 1.2データを受け取り、インスタンス変数に保存
     book = Book.new(book_params)
     # 3.データベースを保存するためのsaveメソッド
-    book.save
-    # 4。index画面へリダイレクト
-    redirect_to book_path(book.id)
+    if book.save
+      # 4。index画面へリダイレクト
+      redirect_to book_path(book.id), notice: 'Book was successfully created.'
+    else
+      render :index
+    end
   end
 
   def show
