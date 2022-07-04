fd0fd23293921e2215142f4b5f5aa915ee117047
EC2 Default User <ec2-user@ip-172-31-19-174.ap-northeast-1.compute.internal>
2022-07-04 13:51:19 +0000

    [add]3-12 全ての機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,12 +7,15 @@ class BooksController < ApplicationController
 
   def create
     # 1.2データを受け取り、インスタンス変数に保存
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
     # 3.データベースを保存するためのsaveメソッド
-    if book.save
+    if @book.save
       # 4。index画面へリダイレクト
-      redirect_to book_path(book.id), notice: 'Book was successfully created.'
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
     else
+      # あくしょんを呼び出さず、そのままindexを表示するので、indexで呼び出すインデックス変数が用意されていない
+      # そのため、render indexの前で、インスタンス変数を用意しておく
+      @books = Book.all
       render :index
     end
   end
@@ -26,9 +29,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: 'Book was successfully update.'
+    else
+      render :edit
+    end
   end
 
   def destroy
